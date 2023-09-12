a49b088f374a0a38940587736fe2122db7b9b1cf
RobAllan27 <rob.m.allan@gmail.com>
2023-09-12 16:28:17 +1000

    Added the modulus element

--- temp1/mathsWithFunctions.js
+++ temp1/mathsWithFunctions.js
@@ -6,9 +6,20 @@ function deductNumbers(number1, number2) {
   return number1 - number2;
 }
 
+function diffBetweenNumbers(number1, number2) {
+  const difference = Math.abs(number1 - number2);
+  return difference;
+}
+
 // Example usage:
 const result1 = addNumbers(2, 1);
 console.log(result1); // Output will be 3
 
 const result2 = deductNumbers(5, 2);
 console.log(result2); // Output will be 3
+
+result3 = diffBetweenNumbers(5, 2);
+console.log(result3); // Output will be 3
+
+result3 = diffBetweenNumbers(4, 7);
+console.log(result3); // Output will be 3
