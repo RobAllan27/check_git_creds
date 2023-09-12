b8ab02e63096f292998bee2c5588c37fa28cff23
RobAllan27 <rob.m.allan@gmail.com>
2023-09-12 14:48:20 +1000

    Here is the second  commit with delete

--- temp1/mathsWithFunctions.js
+++ temp1/mathsWithFunctions.js
@@ -2,6 +2,13 @@ function addNumbers(number1, number2) {
   return number1 + number2;
 }
 
+function deductNumbers(number1, number2) {
+  return number1 - number2;
+}
+
 // Example usage:
-const result = addNumbers(5, 3);
-console.log(result); // Output will be 8
+const result = addNumbers(2, 1);
+console.log(result); // Output will be 3
+
+const result = deductNumbersNumbers(5, 2);
+console.log(result); // Output will be 3
