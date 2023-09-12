7d9afe7759ece8154676c8e4ba14bcf872dca7cc
RobAllan27 <rob.m.allan@gmail.com>
2023-09-12 16:43:37 +1000

    Update mathsWithFunctions.js
    Added an extra hello message in github

--- temp1/mathsWithFunctions.js
+++ temp1/mathsWithFunctions.js
@@ -13,7 +13,7 @@ function diffBetweenNumbers(number1, number2) {
 
 // Example usage:
 const result1 = addNumbers(2, 1);
-console.log(result1); // Output will be 3
+console.log("Hello" + result1); // Output will be 3
 
 const result2 = deductNumbers(5, 2);
 console.log(result2); // Output will be 3
