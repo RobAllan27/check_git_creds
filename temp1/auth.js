b481c3485349ebf336a24425b41bec0b54a35c40
RobAllan27 <rob.m.allan@gmail.com>
2023-09-12 17:54:04 +1000

    Added auth

---
+++ temp1/auth.js
@@ -0,0 +1,19 @@
+// Define the expected username and password
+const expectedUsername = "myusername";
+const expectedPassword = "mypassword";
+
+// Function to check if the entered username and password are valid
+function checkCredentials(username, password) {
+  if (username === expectedUsername && password === expectedPassword) {
+    return "Login successful!";
+  } else {
+    return "Invalid username or password. Please try again.";
+  }
+}
+
+// Example usage:
+const enteredUsername = "myusername";
+const enteredPassword = "mypassword";
+
+const loginResult = checkCredentials(enteredUsername, enteredPassword);
+console.log(loginResult);
