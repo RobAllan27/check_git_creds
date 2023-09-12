e9b30aea01fd03904ef3224161067277365fcacc
RobAllan27 <rob.m.allan@gmail.com>
2023-09-12 16:19:05 +1000

    Maths functions after a bit of unit testing

--- temp1/mathsWithFunctions.js
+++ temp1/mathsWithFunctions.js
@@ -7,8 +7,8 @@ function deductNumbers(number1, number2) {
 }
 
 // Example usage:
-const result = addNumbers(2, 1);
-console.log(result); // Output will be 3
+const result1 = addNumbers(2, 1);
+console.log(result1); // Output will be 3
 
-const result = deductNumbersNumbers(5, 2);
-console.log(result); // Output will be 3
+const result2 = deductNumbers(5, 2);
+console.log(result2); // Output will be 3
---
+++ temp1/mathsWithMoreFunctions.js
@@ -0,0 +1,14 @@
+function multiplyNumbers(number1, number2) {
+  return number1 * number2;
+}
+
+function divideNumbers(number1, number2) {
+  return number1 / number2;
+}
+
+// Example usage:
+const result1 = multiplyNumbers(3, 3);
+console.log(result1); // Output will be 9
+
+const result2 = divideNumbers(4, 2);
+console.log(result2); // Output will be 2
